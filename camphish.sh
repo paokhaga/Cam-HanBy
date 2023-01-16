c0f2fb97a66c66f4be0cf51660b4c0dd3bd659af
paokhaga <96768121+paokhaga@users.noreply.github.com>
2023-01-16 18:06:52 +0700

    Update camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -16,7 +16,7 @@ printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m| (      | (   ) |   | 
 printf "\e[1;92m | (____/\| )   ( || )   ( |\e[0m\e[1;77m| )      | )   ( |___) (___/\____) || )   ( |\e[0m\n"
 printf "\e[1;92m (_______/|/     \||/     \|\e[0m\e[1;77m|/       |/     \|\_______/\_______)|/     \|\e[0m\n"
 printf " \e[1;93m CamPhish Ver 1.5 \e[0m \n"
-printf " \e[1;77m www.techchip.net | youtube.com/techchipnet \e[0m \n"
+printf " \e[1;77m Han-Dev \e[0m \n"
 
 printf "\n"
 
